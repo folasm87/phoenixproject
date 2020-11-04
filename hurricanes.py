62f1117b795f8ec4a876776d39b461bdf937fdb4
Mark Folashade <ayodeji.folashade@gmail.com>
2020-11-04 12:53:53 -0500

    Create hurricanes.py

---
+++ hurricanes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Project: Hurricanes
+
+Team: Phoenix
+
+Name: Mark Folashade
+
+computing ID: mf4us
+
+
+"""
+
+import csv
+import pandas as pd
+import matplotlib.pyplot as plot
