b6077bd74023ba3a6288706d158f2d2c88e3391e
Mark Folashade <ayodeji.folashade@gmail.com>
2020-11-04 12:53:59 -0500

    Create hurricanes_test.py

---
+++ hurricanes_test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 15:39:42 2020
+
+@author: ayode
+"""
+
+import unittest
+import Hurricanes_Class
+
+class HurricanesTestCase(unittest.TestCase):
+
+    """
+    def test_is_gradeAdded_correctly(self):
+        student1 = AStudent('John', '102', 0)
+        student1.enrollInCourse("CS5010")
+        student1.addGrade(89)
+        student1.addGrade(99)
+        grades = [89,99]
+        print(student1.showGrades)
+        self.assertEqual(grades, student1.grades)
+  
+    
+    def test_id(self):
+        student = AStudent('Jane', '101', 0)
+        student.enrollInCourse("CS5010")
+        student.addGrade(89)
+        self.assertEqual('101', student.id)
+    """
+            
+if __name__ == '__main__':
+    unittest.main()       
