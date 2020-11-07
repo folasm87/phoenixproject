06a10a60d0c4676c1f0a3afdca3d8845a00a3f5a
Mark Folashade <ayodeji.folashade@gmail.com>
2020-11-07 13:37:39 -0500

    Update code.py

--- code.py
+++ code.py
@@ -14,23 +14,75 @@ computing ID: mf4us, kt2np, sv8jy, lns4pr
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-#define column name list we're interested in
-columns = ["Name", "Date", "Time", "Event", "Status", "Latitude", "Longitude", "Maximum Wind", "Minimum Pressure"]
 
-#read in data from "atlantic.csv" using the columns we're interested in and skipping rows until year 1950
-df = pd.read_csv("atlantic.csv", usecols = columns, skiprows = range(1,21880))
 
-df.head()
+atlantic_df = pd.read_csv('atlantic.csv')
+
+# set as data frame
+atlantic_df = pd.DataFrame(atlantic_df)
+
+
+
+atlantic_df
+for col in atlantic_df.columns: 
+    print(col)
+
+
+# 1950 onwards, when they began naming storms
+atlantic_df = atlantic_df[atlantic_df['Date'] > 19500000]
+
+atlantic_df = atlantic_df[["ID", "Name", "Date", "Time", "Event", "Status", "Latitude", "Longitude", "Maximum Wind", "Minimum Pressure"]]
+
+
+atlantic_df.groupby('ID').mean()
+
+atlantic_df.groupby('Status').mean()
+
+
+atlantic_df['ID'] = atlantic_df['ID'].str.strip()
+atlantic_df['Name'] = atlantic_df['Name'].str.strip()
+atlantic_df['Event'] = atlantic_df['Event'].str.strip()
+atlantic_df['Status'] = atlantic_df['Status'].str.strip()
+atlantic_df['Longitude'] = atlantic_df['Longitude'].str.strip()
+atlantic_df['Latitude'] = atlantic_df['Latitude'].str.strip()
 
 #add new column for year only
-df['Year'] = df['Date'].astype(str).str[0:4]
-df.head()
+atlantic_df['Year'] = atlantic_df['Date'].astype(str).str[0:4]
+#df.head()
 
 #add new column for month only
-df['Month'] = df['Date'].astype(str).str[4:6]
-df.head()
+atlantic_df['Month'] = atlantic_df['Date'].astype(str).str[4:6]
 
 
-#want to plot number of tropical cyclones each month for all years ?groupby?
+#add new column for day only
+atlantic_df['Day'] = atlantic_df['Date'].astype(str).str[6:8]
+
+
+#cleaning longitude and latitude
+
+atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'N':''}, regex=True)
+atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'S':''}, regex=True)
+atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'W':''}, regex=True)
+atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'E':''}, regex=True)
+
+
+atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'N':''}, regex=True)
+atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'S':''}, regex=True)
+atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'W':''}, regex=True)
+atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'E':''}, regex=True)
+
+
+atlantic_df.dtypes
+
+
+
+#atlantic_df[["Name", "Date", "Time", "Event", "Status", "Latitude", "Longitude", "Maximum Wind", "Minimum Pressure"]]
+
+
+
+atlantic_df.to_csv('atlantic_modified7.csv')
+
+
+#print(df.head())
