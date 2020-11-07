a3f17f0a953258b1821b1b0017680f4daa9bec0a
khtran133 <45692829+khtran133@users.noreply.github.com>
2020-11-07 15:34:24 -0500

    Added cleaning of longitude/latitude data

--- proj1.py
+++ proj1.py
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+import re
+from mpl_toolkits.basemap import Basemap
 
 atlantic_df = pd.read_csv('atlantic.csv')
 
@@ -37,17 +39,36 @@ print(atlantic_df.groupby('Status').mean())
 # start with turning time into strings, to enforce proper %H%M format
 atlantic_df['Time'].unique()
 
-# ints under four digits when converted to strings need to be prepended with 4 - len() zeroes
-for i in range(len(atlantic_df['Time'])):
+## convert longitude and latitude to decimal format for plotting
+# initial datetime conversion in the same loop
+for i in range(len(atlantic_df)):
+    # time
+    # ints under four digits when converted to strings need to be prepended with 4 - len() zeroes
     if len(str(atlantic_df.loc[atlantic_df.index[i], 'Time'])) == 4:
         atlantic_df.loc[atlantic_df.index[i], 'Time'] = str(atlantic_df.loc[atlantic_df.index[i], 'Time'])
     else:
         zeroes = '0' * (4 - len(str(atlantic_df.loc[atlantic_df.index[i], 'Time'])))
         atlantic_df.loc[atlantic_df.index[i], 'Time'] = zeroes + str(atlantic_df.loc[atlantic_df.index[i], 'Time'])
+        
+    # longitude
+    if atlantic_df.loc[atlantic_df.index[i], 'Longitude'][-1] == 'W':
+        atlantic_df.loc[atlantic_df.index[i], 'Longitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Longitude'].replace('W', '')) * -1
+    else:
+        atlantic_df.loc[atlantic_df.index[i], 'Longitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Longitude'].replace('E', ''))
+    
+    # latitude
+    if atlantic_df.loc[atlantic_df.index[i], 'Latitude'][-1] == 'N':
+        atlantic_df.loc[atlantic_df.index[i], 'Latitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Latitude'].replace('N', ''))
+    else:
+        atlantic_df.loc[atlantic_df.index[i], 'Latitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Latitude'].replace('S', '')) * -1
 
+# second loop for creating datetime variable in proper format
 datetime_list = []
 for i in range(len(atlantic_df)):
     datetime_list.append(str(atlantic_df.loc[atlantic_df.index[i], 'Date']) + ' ' + atlantic_df.loc[atlantic_df.index[i], 'Time'])
 
 atlantic_df['Datetime'] = datetime_list
 atlantic_df['Datetime'] = pd.to_datetime(atlantic_df['Datetime'], format = '%Y%m%d %H%M')
+
+# view new datatypes
+print(atlantic_df.dtypes)
