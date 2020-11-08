caa2539460afe6c3cf57e05d4a0dc7a7820ba627
Mark Folashade <ayodeji.folashade@gmail.com>
2020-11-07 20:31:42 -0500

    Update code.py

--- code.py
+++ code.py
@@ -15,19 +15,15 @@ computing ID: mf4us, kt2np, sv8jy, lns4pr
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
+import folium
+from folium import plugins
+import seaborn as sns
+import webbrowser
 
 atlantic_df = pd.read_csv('atlantic.csv')
 
 # set as data frame
 atlantic_df = pd.DataFrame(atlantic_df)
-
-
-
-atlantic_df
-for col in atlantic_df.columns: 
-    print(col)
 
 
 # 1950 onwards, when they began naming storms
@@ -62,32 +58,52 @@ atlantic_df['Day'] = atlantic_df['Date'].astype(str).str[6:8]
 
 #cleaning longitude and latitude
 
-atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'N':''}, regex=True)
-atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'S':''}, regex=True)
-atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'W':''}, regex=True)
-atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'E':''}, regex=True)
+for i in range(len(atlantic_df)):
+    # time
+    # ints under four digits when converted to strings need to be prepended with 4 - len() zeroes
+    if len(str(atlantic_df.loc[atlantic_df.index[i], 'Time'])) == 4:
+        atlantic_df.loc[atlantic_df.index[i], 'Time'] = str(atlantic_df.loc[atlantic_df.index[i], 'Time'])
+    else:
+        zeroes = '0' * (4 - len(str(atlantic_df.loc[atlantic_df.index[i], 'Time'])))
+        atlantic_df.loc[atlantic_df.index[i], 'Time'] = zeroes + str(atlantic_df.loc[atlantic_df.index[i], 'Time'])
+        
+    # longitude
+    if atlantic_df.loc[atlantic_df.index[i], 'Longitude'][-1] == 'W':
+        atlantic_df.loc[atlantic_df.index[i], 'Longitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Longitude'].replace('W', '')) * -1
+    else:
+        atlantic_df.loc[atlantic_df.index[i], 'Longitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Longitude'].replace('E', ''))
+    
+    # latitude
+    if atlantic_df.loc[atlantic_df.index[i], 'Latitude'][-1] == 'N':
+        atlantic_df.loc[atlantic_df.index[i], 'Latitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Latitude'].replace('N', ''))
+    else:
+        atlantic_df.loc[atlantic_df.index[i], 'Latitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Latitude'].replace('S', '')) * -1
 
 
-atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'N':''}, regex=True)
-atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'S':''}, regex=True)
-atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'W':''}, regex=True)
-atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'E':''}, regex=True)
+
+# second loop for creating datetime variable in proper format
+datetime_list = []
+for i in range(len(atlantic_df)):
+    datetime_list.append(str(atlantic_df.loc[atlantic_df.index[i], 'Date']) + ' ' + atlantic_df.loc[atlantic_df.index[i], 'Time'])
+
+atlantic_df['Datetime'] = datetime_list
+atlantic_df['Datetime'] = pd.to_datetime(atlantic_df['Datetime'], format = '%Y%m%d %H%M')
+
+# clean longitude data, as some points are <-180
+# e.g. -359.1 (359.1W) should be 0.9 (0.9E)
+atlantic_df['Longitude'].loc[lambda s: s < -180] = atlantic_df['Longitude'].loc[lambda x: x < -180] + 360
 
 
-atlantic_df.dtypes
+print(atlantic_df.dtypes)
 
 
-
-#atlantic_df[["Name", "Date", "Time", "Event", "Status", "Latitude", "Longitude", "Maximum Wind", "Minimum Pressure"]]
-
-save_df = 'atlantic_modified19.csv'
+save_df = 'atlantic_modified35.csv'
 
 atlantic_df.to_csv(save_df)
 
 
 df = pd.read_csv(save_df)
-print("DF:")
-print(df)
+
 
 #1. How many hurricanes make landfall.
 ## count(select unique ID (or name) by Status = 'HU' and Event = 'L')
@@ -98,34 +114,56 @@ filter_no_landfall = df['Event'] != 'L'
 
 df_1 = df.loc[filter_hurricane&filter_landfall]
 
-print("DF1:")
-print(df_1)
-
 
 n = len(pd.unique(df_1['ID'])) 
   
-print("Number of hurricanes :",  
+print("Number of hurricanes :", n)
+
+
+df_save = df_1.drop_duplicates(subset=['ID'], keep='last', inplace=False)
+save_2 = 'uniq10.csv'
+
+df_save.to_csv(save_2)
+
+
+atlanticDF = df_save[["Latitude", "Longitude"]]
+
+print(atlanticDF)
+
+map_van = folium.Map(location = [37.773972, -122.431297], zoom_start = 13)
+
+
+map_van.add_children(plugins.HeatMap(atlanticDF, radius=15))
+
+#HeatMap of Hurricanes that made landfall
+
+map_van.save("mymap.html")
+webbrowser.open_new_tab("mymap.html")
+
+#print(map_van)
+
+def cmp(x,y):
+     return x==y and len(x)>1
+ 
+def unique(list1): 
+    list_set = set(list1) 
+    # convert the set to the list 
+    unique_list = (list(list_set)) 
+    return unique_list
+
+df_unique_id = unique(df_save['ID'].tolist())
+
+print(df_unique_id)
+n = len(df_unique_id) 
+  
+print("Number of hurricanes(2) :",  
       n)
-
-#save_2 = 'temp3.csv'
-
-#df_2.to_csv(save_2)
-
 
 #2. How many hurricanes reach a certain magnitude, but don’t necessarily make landfall.
 df_2 = df.loc[filter_hurricane&filter_no_landfall]
-
-print("DF2:")
-print(df_2)
-
 
 df_no_landfall = df_2.loc[~df_2['ID'].isin(df_1['ID'])]
 
 n = len(pd.unique(df_no_landfall['ID'])) 
   
-print("Number of hurricanes to reach a certain magnitude but no landfall:",  
-      n)
-print("df_no_landfall:")
-print(df_no_landfall)
-
-#print(df.head())
+print("Number of hurricanes to reach a certain magnitude but no landfall:", n)
