6c242750fb6af1c6030941d1179921f20fa5b1b8
khtran133 <45692829+khtran133@users.noreply.github.com>
2020-11-07 19:51:48 -0500

    Added column for hurricane Category
    Marks non-hurricanes (tropical storms, etc.) as Category 0. Also marks hurricanes below the necessary windspeed threshold for Category 1 (62 knots) as Category 0. Not all Category 0 storms are hurricanes, and vice versa.

--- code.py
+++ code.py
@@ -1,46 +1,28 @@
 # -*- coding: utf-8 -*-
 """
 Project: Hurricanes
-
 Team: Phoenix
-
 Names: Steph Verbout, Mark Folashade, Lindsey Shavers, Khoi Tran
-
-computing ID: mf4us, kt2np, sv8jy, lns4pr
-
-
+Computing IDs: sv8jy, mf4us, lns4pr, kt2np
 """
-
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
+import datetime
 
 atlantic_df = pd.read_csv('atlantic.csv')
 
 # set as data frame
 atlantic_df = pd.DataFrame(atlantic_df)
-
-
-
-atlantic_df
-for col in atlantic_df.columns: 
-    print(col)
-
-
+    
+## cleaning data
 # 1950 onwards, when they began naming storms
 atlantic_df = atlantic_df[atlantic_df['Date'] > 19500000]
-
+# subsetting relevant variables/columns
 atlantic_df = atlantic_df[["ID", "Name", "Date", "Time", "Event", "Status", "Latitude", "Longitude", "Maximum Wind", "Minimum Pressure"]]
-
-
-atlantic_df.groupby('ID').mean()
-
-atlantic_df.groupby('Status').mean()
-
-
+# using str.strip() method on all columns labeled 'object' to remove whitespace for easier sorting/querying later on
+print(atlantic_df.dtypes)
 atlantic_df['ID'] = atlantic_df['ID'].str.strip()
 atlantic_df['Name'] = atlantic_df['Name'].str.strip()
 atlantic_df['Event'] = atlantic_df['Event'].str.strip()
@@ -48,84 +30,87 @@ atlantic_df['Status'] = atlantic_df['Status'].str.strip()
 atlantic_df['Longitude'] = atlantic_df['Longitude'].str.strip()
 atlantic_df['Latitude'] = atlantic_df['Latitude'].str.strip()
 
-#add new column for year only
-atlantic_df['Year'] = atlantic_df['Date'].astype(str).str[0:4]
-#df.head()
+# trying groupby() for exploratory analysis
+print(atlantic_df.groupby('ID').mean())
+print(atlantic_df.groupby('Status').mean())
 
-#add new column for month only
-atlantic_df['Month'] = atlantic_df['Date'].astype(str).str[4:6]
+# process datetime
+# start with turning time into strings, to enforce proper %H%M format
+atlantic_df['Time'].unique()
 
+## convert longitude and latitude to decimal format for plotting
+# initial datetime conversion in the same loop
+for i in range(len(atlantic_df)):
+    # time
+    # ints under four digits when converted to strings need to be prepended with 4 - len() zeroes
+    if len(str(atlantic_df.loc[atlantic_df.index[i], 'Time'])) == 4:
+        atlantic_df.loc[atlantic_df.index[i], 'Time'] = str(atlantic_df.loc[atlantic_df.index[i], 'Time'])
+    else:
+        zeroes = '0' * (4 - len(str(atlantic_df.loc[atlantic_df.index[i], 'Time'])))
+        atlantic_df.loc[atlantic_df.index[i], 'Time'] = zeroes + str(atlantic_df.loc[atlantic_df.index[i], 'Time'])
+        
+    # longitude
+    if atlantic_df.loc[atlantic_df.index[i], 'Longitude'][-1] == 'W':
+        atlantic_df.loc[atlantic_df.index[i], 'Longitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Longitude'].replace('W', '')) * -1
+    else:
+        atlantic_df.loc[atlantic_df.index[i], 'Longitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Longitude'].replace('E', ''))
+    
+    # latitude
+    if atlantic_df.loc[atlantic_df.index[i], 'Latitude'][-1] == 'N':
+        atlantic_df.loc[atlantic_df.index[i], 'Latitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Latitude'].replace('N', ''))
+    else:
+        atlantic_df.loc[atlantic_df.index[i], 'Latitude'] = float(atlantic_df.loc[atlantic_df.index[i], 'Latitude'].replace('S', '')) * -1
 
-#add new column for day only
-atlantic_df['Day'] = atlantic_df['Date'].astype(str).str[6:8]
+# second loop for creating datetime and category variables
+# use 0 for non-hurricanes (tropical storms, etc.)
+# 1: Maximum W 64 <= x < 82
+# 2: 82 <= x < 95
+# 3: 95 <= x < 112
+# 4: 112 <= x < 136
+# 5: x >= 136
 
-
-#cleaning longitude and latitude
-
-atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'N':''}, regex=True)
-atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'S':''}, regex=True)
-atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'W':''}, regex=True)
-atlantic_df['Longitude'] = atlantic_df['Longitude'].replace({'E':''}, regex=True)
-
-
-atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'N':''}, regex=True)
-atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'S':''}, regex=True)
-atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'W':''}, regex=True)
-atlantic_df['Latitude'] = atlantic_df['Latitude'].replace({'E':''}, regex=True)
-
-
-atlantic_df.dtypes
-
-
-
-#atlantic_df[["Name", "Date", "Time", "Event", "Status", "Latitude", "Longitude", "Maximum Wind", "Minimum Pressure"]]
-
-save_df = 'atlantic_modified19.csv'
-
-atlantic_df.to_csv(save_df)
-
-
-df = pd.read_csv(save_df)
-print("DF:")
-print(df)
-
-#1. How many hurricanes make landfall.
-## count(select unique ID (or name) by Status = 'HU' and Event = 'L')
-
-filter_hurricane = df['Status']=='HU'
-filter_landfall = df['Event']=='L'
-filter_no_landfall = df['Event'] != 'L'
-
-df_1 = df.loc[filter_hurricane&filter_landfall]
-
-print("DF1:")
-print(df_1)
-
-
-n = len(pd.unique(df_1['ID'])) 
-  
-print("Number of hurricanes :",  
-      n)
-
-#save_2 = 'temp3.csv'
-
-#df_2.to_csv(save_2)
-
-
-#2. How many hurricanes reach a certain magnitude, but don’t necessarily make landfall.
-df_2 = df.loc[filter_hurricane&filter_no_landfall]
-
-print("DF2:")
-print(df_2)
+datetime_list = []
+category_list = []
+for i in range(len(atlantic_df)):
+    datetime_list.append(str(atlantic_df.loc[atlantic_df.index[i], 'Date']) + ' ' + atlantic_df.loc[atlantic_df.index[i], 'Time'])
+    
+    if atlantic_df.loc[atlantic_df.index[i], 'Status'] != 'HU':
+        category_list.append(0)
+    else:
+        if atlantic_df.loc[atlantic_df.index[i], 'Maximum Wind'] > 136:
+            category_list.append(5)
+        elif atlantic_df.loc[atlantic_df.index[i], 'Maximum Wind'] > 112:
+            category_list.append(4)
+        elif atlantic_df.loc[atlantic_df.index[i], 'Maximum Wind'] > 95:
+            category_list.append(3)
+        elif atlantic_df.loc[atlantic_df.index[i], 'Maximum Wind'] > 82:
+            category_list.append(2)
+        elif atlantic_df.loc[atlantic_df.index[i], 'Maximum Wind'] > 62:
+            category_list.append(1)
+        else:
+            category_list.append(0)
+    
+atlantic_df['Datetime'] = datetime_list
+atlantic_df['Datetime'] = pd.to_datetime(atlantic_df['Datetime'], format = '%Y%m%d %H%M')
+atlantic_df['Category'] = category_list
 
 
-df_no_landfall = df_2.loc[~df_2['ID'].isin(df_1['ID'])]
+# clean longitude data, as some points are <-180
+# e.g. -359.1 (359.1W) should be 0.9 (0.9E)
+atlantic_df['Longitude'].loc[lambda s: s < -180] = atlantic_df['Longitude'].loc[lambda x: x < -180] + 360
 
-n = len(pd.unique(df_no_landfall['ID'])) 
-  
-print("Number of hurricanes to reach a certain magnitude but no landfall:",  
-      n)
-print("df_no_landfall:")
-print(df_no_landfall)
+# view min and max longitude and latitude points
+# use these figures to download a map from openstreetmap.org
+boundaries = ((atlantic_df.Longitude.min(), atlantic_df.Longitude.max(), atlantic_df.Latitude.min(), atlantic_df.Latitude.max()))
 
-#print(df.head())
+hurricane_map = plt.imread('map.png')
+fig, ax = plt.subplots(figsize = (8, 8))
+ax.scatter(atlantic_df.Longitude, atlantic_df.Latitude, zorder = 1, alpha = 0.2, c = 'b', s = 10)
+ax.set_title('Plotting Hurricane Data on the Atlantic Ocean Map')
+# axis limits for plot set to min and max figures for latitude and longitude
+ax.set_xlim(boundaries[0], boundaries[1])
+ax.set_ylim(boundaries[2], boundaries[3])
+ax.imshow(hurricane_map, zorder = 0, extent = boundaries, aspect = 'auto')
+
+# saving dataset
+# atlantic_df.to_csv('atlantic_hurricanes.csv')
